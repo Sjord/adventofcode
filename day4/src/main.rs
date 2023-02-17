f87dcac1bf8e2456de483f801b793b713aeaaeba
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-02-17 17:12:17 +0100

    Day 4 part 1

---
+++ day4/src/main.rs
@@ -0,0 +1,35 @@
+use std::{env, fs, ops::RangeInclusive};
+
+
+fn main() {
+    let fname = env::args().nth(1).unwrap();
+    let contents = fs::read_to_string(fname).unwrap();
+    let pairs = contents.lines().map(|l| {
+        let mut ranges = l.split(',').map(|r| {
+            let nums : Vec<u8> = r.split('-').map(|n| n.parse().unwrap()).collect();
+            RangeInclusive::new(nums[0], nums[1])
+        });
+        Pair { 0: ranges.next().unwrap(), 1: ranges.next().unwrap() }
+    });
+    let count = pairs.filter(|p| p.overlaps()).count();
+    println!("{}", count);
+}
+
+struct Pair (RangeInclusive<u8>, RangeInclusive<u8>);
+
+impl Pair {
+    fn overlaps(&self) -> bool {
+        self.0.overlaps(&self.1) || self.1.overlaps(&self.0)
+    }
+}
+
+trait Overlaps {
+    fn overlaps(&self, other: &Self) -> bool;
+}
+
+
+impl Overlaps for RangeInclusive<u8> {
+    fn overlaps(&self, other: &RangeInclusive<u8>) -> bool {
+        self.start() <= other.start() && self.end() >= other.end()
+    }
+}
