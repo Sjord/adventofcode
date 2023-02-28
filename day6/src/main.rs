e03f8635a5abfca6543a5629a8ab8db3c03ed52e
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-02-28 12:29:34 +0100

    Day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,12 +1,10 @@
-use std::{env, fs};
-
-use ringbuffer::{AllocRingBuffer, RingBufferWrite, RingBuffer, RingBufferRead, RingBufferExt};
+use std::{env, fs, slice::Iter};
 
 fn main() {
     let fname = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(fname).unwrap();
 
-    let mut buf = AllocRingBuffer::<char>::with_capacity(4);
+    let mut buf = RingBuffer::<char>::with_capacity(14);
     for (i, ch) in contents.chars().enumerate() {
         buf.push(ch);
         if is_unique(&buf) {
@@ -16,17 +14,51 @@ fn main() {
     }
 }
 
-fn is_unique(buf: &AllocRingBuffer::<char>) -> bool {
-    if buf.len() < 4 {
+fn is_unique(buf: &RingBuffer::<char>) -> bool {
+    if buf.len() < buf.capacity {
         return false;
     }
 
     for (i, a) in buf.iter().enumerate() {
         for (j, b) in buf.iter().enumerate() {
+            // println!("{} {} {} {}", i, j, a, b);
             if (i != j && a == b) {
                 return false;
             }
         }
     }
     true
+}
+
+struct RingBuffer<T> {
+    capacity: usize,
+    buffer: Vec<T>,
+    writer: usize,
+}
+
+impl<T> RingBuffer<T> {
+    fn with_capacity(capacity: usize) -> RingBuffer<T> {
+        Self {
+            capacity,
+            buffer: Vec::with_capacity(capacity),
+            writer: 0
+        }
+    }
+
+    fn iter(&self) -> Iter<T> {
+        self.buffer.iter()
+    }
+
+    fn push(&mut self, elem: T) {
+        if self.writer >= self.buffer.len() {
+            self.buffer.push(elem);
+        } else {
+            self.buffer[self.writer] = elem;
+        }
+        self.writer = (self.writer + 1) % self.capacity;
+    }
+
+    fn len(&self) -> usize {
+        self.buffer.len()
+    }
 }
