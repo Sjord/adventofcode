2ba525dc2577a3c15ddbdd0cf701f77d68308295
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-03-10 14:19:45 +0100

    Correct throwing

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -7,7 +7,22 @@ use parser::monkeys;
 fn main() {
     let fname = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(fname).unwrap();
-    let monkeys = monkeys(&contents).unwrap();
+    let mut monkeys = monkeys(&contents).unwrap();
+    println!("{:?}", monkeys);
+
+    for round in 0..20 {
+        println!("Round {}", round);
+            for i in 0..monkeys.len() {
+            println!("  Monkey {}", i);
+            let monkey = &mut monkeys[i];
+            let throws = monkey.inspect_and_throw();
+            for throw in throws {
+                println!("    Throws {} to {}", throw.item, throw.destination);
+                monkeys[throw.destination as usize].items.push(throw.item);
+            }
+        }
+    }
+    
     println!("{:?}", monkeys);
 }
 
@@ -35,8 +50,49 @@ enum Operator {
     Multiply
 }
 
+impl Operation {
+    fn evaluate(&self, old: i32) -> i32 {
+        match self.operator {
+            Operator::Add => self.left.value(old) + self.right.value(old),
+            Operator::Multiply => self.left.value(old) * self.right.value(old),
+        }
+    }
+}
+
 #[derive(Debug)]
 enum Term {
     Old,
     Const(i32)
+}
+
+impl Term {
+    fn value(&self, old: i32) -> i32 {
+        match self {
+            Term::Old => old,
+            Term::Const(i) => *i
+        }
+    }
+}
+
+impl Monkey {
+    fn inspect_and_throw(&mut self) -> Vec<ThrownItem> {
+        let mut result = Vec::with_capacity(self.items.len());
+        for item in self.items.iter() {
+            let item = self.operation.evaluate(*item);
+            let item = item / 3;
+            let destination = if item % self.test == 0 {
+                self.trueMonkey
+            } else {
+                self.falseMonkey
+            };
+            result.push(ThrownItem { destination, item });
+        };
+        self.items.clear();
+        result
+    }
+}
+
+struct ThrownItem {
+    destination: i32,
+    item: i32
 }
