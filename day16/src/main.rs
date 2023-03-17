9dd88bf960d9418265729faa55138cb1a6b4ce95
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-03-17 12:01:49 +0100

    Day 16 part 1

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -8,12 +8,96 @@ use nom::{
     sequence::{preceded, tuple},
     IResult,
 };
+use petgraph::{dot::{Config, Dot}, algo::dijkstra};
+use petgraph::prelude::UnGraphMap;
 
 fn main() {
     let fname = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(fname).unwrap();
     let records = parse(&contents);
-    dbg!(records);
+
+    let mut graph = UnGraphMap::<Valve, ()>::new();
+    for r in &records {
+        graph.add_node(Valve {
+            name: r.name,
+            flow_rate: r.flow_rate,
+        });
+    }
+    for r in records {
+        for tunnel in r.tunnels {
+            let from = graph.nodes().find(|n| n.name == r.name).unwrap();
+            let to = graph.nodes().find(|n| n.name == tunnel).unwrap();
+            graph.add_edge(from, to, ());
+        }
+    }
+
+    let start = graph.nodes().find(|n| n.name == "AA").unwrap();
+    let mut search = WalkState {
+        graph,
+        position: start,
+        minutes_left: 30,
+        released_pressure: 0,
+        open_valves: Vec::new(),
+    };
+    let optimal = search.search();
+    dbg!(optimal);
+}
+
+#[derive(Clone)]
+struct WalkState<'a> {
+    graph: UnGraphMap<Valve<'a>, ()>,
+    position: Valve<'a>,
+    minutes_left: i32,
+    released_pressure: i32,
+    open_valves: Vec<Valve<'a>>,
+}
+
+impl<'a> WalkState<'a> {
+    fn search(&mut self) -> i32 {
+        if self.minutes_left <= 0 {
+            return self.released_pressure;
+        }
+
+        let candidates = self
+            .graph
+            .nodes()
+            .filter(|n| *n != self.position && n.flow_rate > 0 && !self.open_valves.contains(n));
+        candidates
+            .map(|c| {
+                let mut state = self.clone();
+                state.walk_to(c);
+                state.turn_valve(c);
+                state.search()
+            })
+            .max()
+            .or_else(|| {
+                while self.minutes_left > 0 {
+                    self.minute_passed();
+                }
+                Some(self.released_pressure)
+            })      
+            .unwrap()
+    }
+
+    fn walk_to(&mut self, destination: Valve<'a>) {
+        let distance = *dijkstra(&self.graph, self.position, Some(destination), |_| 1).get(&destination).unwrap();
+        for step in 0..distance {
+            self.minute_passed();
+        }
+        self.position = destination;
+    }
+
+    fn turn_valve(&mut self, valve: Valve<'a>) {
+        self.minute_passed();
+        self.open_valves.push(valve);
+    }
+
+    fn minute_passed(&mut self) {
+        self.minutes_left -= 1;
+        if self.minutes_left >= 0 {
+            self.released_pressure += self.open_valves.iter().map(|v| v.flow_rate).sum::<i32>();
+        }
+    }
 }
 
 fn parse(input: &str) -> Vec<ValveRecord> {
@@ -21,8 +105,7 @@ fn parse(input: &str) -> Vec<ValveRecord> {
 }
 
 fn parse_valve(input: &str) -> IResult<&str, ValveRecord> {
-    let (i, (_, name, _, flow_rate, _, tunnels)) =
-    tuple((
+    let (i, (_, name, _, flow_rate, _, tunnels)) = tuple((
         tag("Valve "),
         valve_name,
         tag(" has flow rate="),
@@ -33,7 +116,14 @@ fn parse_valve(input: &str) -> IResult<&str, ValveRecord> {
         )),
         valve_names,
     ))(input)?;
-    Ok((i, ValveRecord { name, flow_rate, tunnels }))
+    Ok((
+        i,
+        ValveRecord {
+            name,
+            flow_rate,
+            tunnels,
+        },
+    ))
 }
 
 fn valve_names(input: &str) -> IResult<&str, Vec<&str>> {
@@ -48,5 +138,23 @@ fn valve_name(input: &str) -> IResult<&str, &str> {
 struct ValveRecord<'a> {
     name: &'a str,
     flow_rate: i32,
-    tunnels: Vec<&'a str>
+    tunnels: Vec<&'a str>,
+}
+
+#[derive(Debug, Hash, Clone, Copy, Eq, PartialOrd)]
+struct Valve<'a> {
+    name: &'a str,
+    flow_rate: i32,
+}
+
+impl<'a> Ord for Valve<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.name.cmp(&other.name)
+    }
+}
+
+impl<'a> PartialEq for Valve<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
 }
