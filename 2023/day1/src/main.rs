e91ef353be303e64e389eba67964d68dde288e74
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-02-17 11:42:39 +0100

    Incorrect attempt at day 1 part 2

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,14 +1,43 @@
+use std::collections::HashMap;
 use std::fs;
 use std::env;
+use regex::Regex;
+use regex::Match;
 
 fn main() {
     let fname = env::args().nth(1).unwrap();
     let a = fs::read_to_string(fname).unwrap().lines().fold(0, |acc, l| {
-        let digits : Vec<char> = l.chars().filter(|c| c.is_digit(10)).collect();
-        let mut number: String = String::new();
-        number.push(*digits.first().unwrap());
-        number.push(*digits.last().unwrap());
-        acc + number.parse::<i64>().unwrap()
+        acc + get_number(l)
     });
     dbg!(a);
+}
+
+fn get_number(line: &str) -> i64 {
+    let replacements = HashMap::from([
+        ("one", "1"), 
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+    let keys_and_values = replacements.keys().chain(replacements.values());
+    let re_str = keys_and_values.copied().collect::<Vec<_>>().join("|");
+    let re = Regex::new(&re_str).unwrap();
+    let matches : Vec<Match> = re.find_iter(line).collect();
+    
+    let mut first = matches.first().unwrap().as_str();
+    if replacements.contains_key(first) {
+        first = replacements[first];
+    }
+
+    let mut last = matches.last().unwrap().as_str();
+    if replacements.contains_key(last) {
+        last = replacements[last];
+    }
+
+    (first.to_owned() + last).parse().unwrap()
 }
