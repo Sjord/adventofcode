7eff525870064b00f0cd26896f85f8137de2a181
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-02-17 12:20:01 +0100

    Working solution

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,20 +1,22 @@
 use std::collections::HashMap;
-use std::fs;
 use std::env;
-use regex::Regex;
-use regex::Match;
+use std::fs;
 
 fn main() {
     let fname = env::args().nth(1).unwrap();
-    let a = fs::read_to_string(fname).unwrap().lines().fold(0, |acc, l| {
-        acc + get_number(l)
-    });
+    let a = fs::read_to_string(fname)
+        .unwrap()
+        .lines()
+        .fold(0, |acc, l| {
+            println!("{} {}", get_number(l), l);
+            acc + get_number(l)
+        });
     dbg!(a);
 }
 
 fn get_number(line: &str) -> i64 {
     let replacements = HashMap::from([
-        ("one", "1"), 
+        ("one", "1"),
         ("two", "2"),
         ("three", "3"),
         ("four", "4"),
@@ -25,19 +27,23 @@ fn get_number(line: &str) -> i64 {
         ("nine", "9"),
     ]);
     let keys_and_values = replacements.keys().chain(replacements.values());
-    let re_str = keys_and_values.copied().collect::<Vec<_>>().join("|");
-    let re = Regex::new(&re_str).unwrap();
-    let matches : Vec<Match> = re.find_iter(line).collect();
-    
-    let mut first = matches.first().unwrap().as_str();
+    let mut first = keys_and_values
+        .min_by_key(|v| line.find(*v).unwrap_or(999))
+        .unwrap()
+        .to_owned();
     if replacements.contains_key(first) {
         first = replacements[first];
     }
 
-    let mut last = matches.last().unwrap().as_str();
+    let keys_and_values = replacements.keys().chain(replacements.values());
+    let mut last = keys_and_values
+        .max_by_key(|v| line.rfind(*v).unwrap_or(0))
+        .unwrap()
+        .to_owned();
     if replacements.contains_key(last) {
         last = replacements[last];
     }
 
-    (first.to_owned() + last).parse().unwrap()
+    let number = first.to_owned() + last;
+    number.parse().unwrap()
 }
