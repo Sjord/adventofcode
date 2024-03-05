bf02d8df915615eb10ba95322af4ec212fe7f1cd
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-03-05 13:57:44 +0100

    Day 6 part 2

--- 2023/day6/src/main.rs
+++ 2023/day6/src/main.rs
@@ -4,18 +4,12 @@ fn main() {
     let fname = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(fname).unwrap();
     let mut lines = contents.lines().map(|l| l.split_ascii_whitespace());
-    let times : Vec<i64> = lines.next().unwrap().skip(1).map(|t| t.parse().unwrap()).collect();
-    let distances : Vec<i64> = lines.next().unwrap().skip(1).map(|t| t.parse().unwrap()).collect();
+    let time : i64 = lines.next().unwrap().skip(1).fold(String::new(), |a, b| a + b).parse().unwrap();
+    let distance : i64 = lines.next().unwrap().skip(1).fold(String::new(), |a, b| a + b).parse().unwrap();
 
-    let mut result = 1;
-    for i in 0..times.len() {
-        let time = times[i];
-        let distance = distances[i];
-        let limits = calc_winning_limits(time, distance);
-        let options = 1 + limits.1 - limits.0;
-        result *= options;
-    }
-    dbg!(result);
+    let limits = calc_winning_limits(time, distance);
+    let options = 1 + limits.1 - limits.0;
+    dbg!(options);
 }
 
 fn calc_winning_limits(time: i64, distance: i64) -> (i64, i64) {
