2d1fa084ae196c051d0c70f7a0cb949d4966b0f0
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-03-11 13:58:41 +0100

    Day 8 part 2

--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -1,6 +1,9 @@
 use std::{env, fs};
 
-use nom::{bytes::complete::tag, character::complete::{alpha1, newline, one_of}, multi::{many_till, separated_list1}, sequence::tuple, IResult};
+use nom::{bytes::complete::tag, character::complete::{alphanumeric1, newline, one_of}, multi::{many_till, separated_list1}, sequence::tuple, IResult};
+
+use  num::integer::lcm;
+use num::Integer;
 
 fn main() {
     let fname = env::args().nth(1).unwrap();
@@ -22,6 +25,23 @@ struct Node {
     right: String
 }
 
+impl Node {
+    fn is_start(&self) -> bool {
+        self.name.ends_with('A')
+    }
+
+    fn is_end(&self) -> bool {
+        self.name.ends_with('Z')
+    }
+
+    fn get_leg(&self, direction: &Direction) -> &str {
+        match direction {
+            Direction::Left => &self.left,
+            Direction::Right => &self.right
+        }
+    }
+}
+
 #[derive(Debug)]
 enum Direction {
     Left,
@@ -30,15 +50,18 @@ enum Direction {
 
 impl Map {
     fn walk(&self) -> i64 {
+        let starts = self.nodes.iter().filter(|n| n.is_start());
+        let steps = starts.map(|s| self.walk_from_node(s));
+        steps.reduce(|a, b| a.lcm(&b)).unwrap()
+    }
+
+    fn walk_from_node(&self, node: &Node) -> i64{
         let mut step_ctr = 0;
         let mut dir_pointer = 0;
-        let mut current = self.find_node("AAA");
-        while current.name != "ZZZ" {
+        let mut current = node;
+        while !current.is_end() {
             let direction = &self.directions[dir_pointer];
-            let next = match direction {
-                Direction::Left => &current.left,
-                Direction::Right => &current.right
-            };
+            let next = current.get_leg(direction);
             current = self.find_node(next);
             dir_pointer = (dir_pointer + 1) % self.directions.len();
             step_ctr += 1;
@@ -47,7 +70,7 @@ impl Map {
     }
 
     fn find_node(&self, name: &str) -> &Node {
-        self.nodes.iter().find(|n| n.name == name).unwrap()
+        self.nodes.iter().find(|n| n.name == name).expect(name)
     }
 }
 
@@ -62,7 +85,7 @@ fn parse_direction(input: &str) -> IResult<&str, Direction> {
 }
 
 fn parse_node(input: &str) -> IResult<&str, Node> {
-    let mut parser = tuple((alpha1, tag(" = ("), alpha1, tag(", "), alpha1, tag(")")));
+    let mut parser = tuple((alphanumeric1, tag(" = ("), alphanumeric1, tag(", "), alphanumeric1, tag(")")));
     let (remainder, parsed) = parser(input)?;
     let (name, _, left, _, right, _) = parsed;
     Ok((remainder, Node {
